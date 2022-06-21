7b7df6d29d1c2a953bc90fefcac16a3bd50d68eb
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-06-22 01:17:31 +0300

    add player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const player = new Player('vimeo-player');
 
+const LOCAL_STORAGE = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(LOCAL_STORAGE);
+currentTime !== null ? player.setCurrentTime(currentTime) : 0;
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    currentTime = localStorage.setItem(LOCAL_STORAGE, time.seconds);
+  }, 1000)
+);
