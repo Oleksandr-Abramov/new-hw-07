709d7db15593516481659c1b32fbfc6d3647abd2
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-06-23 17:25:14 +0300

    add form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,3 +12,61 @@ player.on(
     currentTime = localStorage.setItem(LOCAL_STORAGE, time.seconds);
   }, 1000)
 );
+// const namesOfMonth = [
+//   'Січень',
+//   'Лютий',
+//   'Березень',
+//   'Квітень',
+//   'Травень',
+//   'Червень',
+//   'Липень',
+//   'Серпень',
+//   'Вересень',
+//   'Жовтень',
+//   'Листопад',
+//   'Грудень',
+// ];
+// const namesOfDay = [
+//   'Неділя',
+//   'Понеділок',
+//   'Вівторок',
+//   'Середа',
+//   'Четвер',
+//   'Пятниця',
+//   'Субота',
+// ];
+// const month = document.querySelector('.date-month');
+// const date = document.querySelector('.date');
+// const day = document.querySelector('.date-day');
+// const year = document.querySelector('.date-year');
+// const time = document.querySelector('.digital-clock');
+
+// function getActiveTime() {
+//   const time = new Date();
+//   getYear(time);
+//   getMonth(time);
+//   getDay(time);
+//   getDate(time);
+//   hour(time);
+// }
+// setInterval(getActiveTime, 1000);
+
+// function getYear(time) {
+//   year.textContent = time.getFullYear();
+// }
+// function getMonth(time) {
+//   month.textContent = namesOfMonth[time.getMonth()];
+// }
+// function getDay(time) {
+//   day.textContent = namesOfDay[time.getDay()];
+// }
+// function getDate(time) {
+//   date.textContent = time.getDate();
+// }
+
+// function hour(times) {
+//   const hours = times.getHours();
+//   const minutes = times.getMinutes();
+//   const seconds = times.getSeconds();
+//   time.textContent = `${hours} : ${minutes} : ${seconds}`;
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const emailForm = form.email;
+const messageForm = form.message;
+
+let formData = {};
+
+formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+if (formData !== {} && formData !== null) {
+  const { email, message } = formData;
+  emailForm.value = email;
+  messageForm.value = message;
+}
+
+function onInputForm() {
+  formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  console.log('~ formData', formData);
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  form.reset();
+  console.log(formData);
+}
